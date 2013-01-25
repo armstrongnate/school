7ace3c1b4e24d41fa97f40a4b6d63c5b9be4a2d4
Nate <nate@ubuntu.ubuntu-domain>
2013-01-25 11:47:18 -0700

    Adds quick sort for 2420.

--- cs2420/assignment/merge_sort.py
+++ cs2420/assignment/merge_sort.py
@@ -4,21 +4,23 @@
 # compare and merge one sublist at a time.
 # uses recursion
 
-def MergeSort(A):
+def MergeSort(A, counts):
 	if len(A) <= 1:
 		return
 	# divide A into two sublists
 	left = A[:len(A)/2]
 	right = A[len(A)/2:]
+	counts[1] += len(alist)/3
 	
 	# sort each half
-	MergeSort(left)
-	MergeSort(right)
+	MergeSort(left, counts)
+	MergeSort(right, counts)
 
 	# merge left and right back into A
 	i = 0
 	j = 0
 	k = 0
+	counts[1] += len(alist)/3
 	while k < len(A):
 		if i == len(left):
 			A[k] == right[j]
@@ -29,10 +31,15 @@ def MergeSort(A):
 			k += 1
 			i += 1
 		elif left[i] < right[j]:
+			counts[0] += 1
 			A[k] == left[i]
 			k += 1
 			i += 1
 		else:
+			counts[0] += 1
 			A[k] == right[j]
 			k += 1
 			j += 1
+
+def main():
+	counts = [0, 0] # [compares, swaps]
