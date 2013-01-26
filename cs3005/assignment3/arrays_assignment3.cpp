5f8de9fb42e6b3b6141aef70129fbf6718ac3351
Nate <nate@ubuntu.ubuntu-domain>
2013-01-25 23:51:15 -0700

    Almost finishes cs3005 A3. Binary search still needs work.

--- cs3005/assignment3/arrays_assignment3.cpp
+++ cs3005/assignment3/arrays_assignment3.cpp
@@ -17,13 +17,53 @@ int find_smallest_item(int numbers[], int length)
 
 int linear_search(int numbers[], int length, int lost)
 {
+  int found;
   for (int i=0; i<length; i++)
   {
     if (numbers[i] == lost)
       return i;
-    }
   }
   return -1;
+}
+
+void sort_array(int numbers[], int length)
+{
+  bool switched = true;
+  while (switched)
+  {
+    switched = false;
+    for (int i=0; i<length-1; i++)
+    {
+      if (numbers[i] > numbers[i+1])
+      {
+        int temp = numbers[i];
+        numbers[i] = numbers[i+1];
+        numbers[i+1] = temp;
+        switched = true;
+      }
+    }
+  }
+}
+
+void display_list(int list[], int length)
+{
+  for (int i=0; i<length; i++)
+    std::cout << list[i] << " ";
+}
+
+int binary_search(int list[], int length, int a, int high=5, int low=0, int m=3)
+{
+  high = length;
+  low = 0;
+  m = length/2;
+  if (list[m] > a)
+    binary_search(list, length, a, length, m+1);
+  else if (list[m] < a)
+    binary_search(list, length, a, m-1, 0);
+  else
+    return m;
+  std::cout << "found at index: " << m << std::endl;
+
 }
 
 int main(int argc, char *argv[])
@@ -51,4 +91,15 @@ int main(int argc, char *argv[])
   std::cin >> lost;
   int found_at = linear_search(numbers, length, lost);
   std::cout << "Your number is at index: " << found_at << std::endl;
+
+  // ----- Sort an array in ascending order ----- //
+  sort_array(numbers, length);
+  std::cout << "Sorted Array:" << std::endl;
+  display_list(numbers, length);
+
+  // ----- Binary search an ascending order sorted array ----- //
+  int a;
+  std::cout << "Binary search for what number? ";
+  std::cin >> a;
+  binary_search(numbers, length, a);
 }
