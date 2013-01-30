a323e1f40c8d22a4a44e93aa82c89d598fe0950b
COMPAQ <compaq@ubuntu.ubuntu-domain>
2013-01-29 22:21:23 -0700

    adds test to hash sort.

--- cs2420/assignment/hash_sort.py
+++ cs2420/assignment/hash_sort.py
@@ -3,20 +3,37 @@
 # create a frequency chart
 
 # data member to index
+import random
 
-A = [5,4,2,6,4,2,3]
-print A
-F = []
-for i in range(len(A)):
-  F.append(0)
-for n in A:
-  F[n] += 1
+def CreateRandomList(listAmount=10):
+  l = []
+  for i in range(listAmount):
+    l.append(random.randrange(0,listAmount))
+  return l
 
-i = 0
-for n in range(len(F)):
-  if F[n] > 0:
-    for j in range(i, i+F[n]):
-      A[j] = n
-    i += F[n]
+def Compare(list1, list2):
+  for i in range(len(list1)):
+    if list1[i] != list2[i]:
+      print "Error at index: %s" % i
+      return
+  print "Success!"
+ 
+def main():   
+  A = CreateRandomList(10000)
+  Acopy = A[:]
+  Acopy.sort()
+  F = []
+  for i in range(len(A)):
+    F.append(0)
+  for n in A:
+    F[n] += 1
 
-print A
+  i = 0
+  for n in range(len(F)):
+    if F[n] > 0:
+      for j in range(i, i+F[n]):
+        A[j] = n
+      i += F[n]
+  Compare(A, Acopy)
+
+main()
