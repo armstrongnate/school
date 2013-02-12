7ad7d0dddd302a313b0dd045eb747f78c5e1e78e
Nate <natearmstrong2@gmail.com>
2013-02-11 20:11:15 -0700

    finishes 2420 Student program.

--- cs2420/program3/student.py
+++ cs2420/program3/student.py
@@ -8,6 +8,9 @@ class Student:
     self.mEmail = words[3]
     self.mAge = int(words[4])
 
+  def getAge(self):
+    return self.mAge
+
   def __str__(self):
     return "%s, %s" % (self.mLast.capitalize(), self.mFirst.capitalize())
 
@@ -18,11 +21,8 @@ class Student:
 
   # TODO: write ne, lt, gt, le, ge
 
-
-def main():
+def Insert(allStudents):
   before = time.time()
-  allStudents = []
-  # ===== Insert ===== #
   fin = open("insert_names.txt", "r")
   for line in fin:
     words = line.split() # gets the 5 items in a list, each item at its own index.
@@ -36,21 +36,65 @@ def main():
       allStudents.append(s)
 
   after = time.time()
-  print "Time taken: %.2f minutes." % ((after - before)/60)
-  print "Number of Students: %s" % len(allStudents)
+  print "Time taken to insert: %.2f minutes." % ((after - before)/60)
+  print "Number of Students: %s\n" % len(allStudents)
 
-main()
-
-  # ===== Traverse ===== #
-  ##totalAge = 0.0
+def Traverse(allStudents):
+  before = time.time()
+  totalAge = 0.0
   # should end up being 42.something
-  ##for s in allStudnets:
-    ##totalAge += s.getAge()
-  ##print "The average age of all the students is %f" % (totalAge/len(allStudents))
+  for s in allStudents:
+    totalAge += s.getAge()
+  after = time.time()
+  print "The average age of all the students is %f" % (totalAge/len(allStudents))
+  print "Time taken to traverse: %.2f seconds.\n" % (after - before)
 
-  # ===== Delete ====== #
-  # ssn = line.strip()
+def Delete(allStudents):
+  before = time.time()
+  fin = open("delete_names.txt", "r")
+  for line in fin:
+    ssn = line.strip()
+    blank_student = Student(["", "", ssn, "", "25"])
+    found = False
+    for i in range(len(allStudents)):
+      if allStudents[i] == blank_student:
+        found = True
+        allStudents.pop(i)
+        break
+    if not found:
+      print "No student found with SSN %s." % ssn 
+  after = time.time()
+  print "Number of Students, now: %s" % len(allStudents)
+  print "Time taken to delete: %.2f seconds.\n" % (after - before)
 
+def Retrieve(allStudents):
+  before = time.time()
+  fin = open("retrieve_names.txt", "r")
+  totalAge = 0.0
+  count = 0
+  for line in fin:
+    ssn = line.strip()
+    blank_student = Student(["", "", ssn, "", "25"])
+    found = False
+    for i in range(len(allStudents)):
+      if allStudents[i] == blank_student:
+        found = True
+        totalAge += allStudents[i].getAge()
+        count += 1
+        break
+    if not found:
+      print "No student found with SSN %s." % ssn 
+  after = time.time()
+  print "Average age of retrieved students: %f" % (totalAge/count)
+  print "Time taken to retrieve: %.2f seconds.\n" % (after - before)
+
+def main():
+  allStudents = []
+  Insert(allStudents)
+  Traverse(allStudents)
+  Retrieve(allStudents)
+  Delete(allStudents)
   # ===== Retrieve ===== #
   # totalAge should end up being 49.something
   # use count to keep track of how many were totally retrieved so you can divide by that number to find average.
+main()
