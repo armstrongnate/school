d5a8bea346b79768ac24377070d6f629056c6bb4
Nate <natearmstrong2@gmail.com>
2013-02-25 18:32:20 -0700

    finishes assembler part1.

--- cs2810/assembler/assembler.py
+++ cs2810/assembler/assembler.py
@@ -1,4 +1,5 @@
 # helper functions
+from Tables import *
 SYMBOLS = ['_', '.', '$', ':']
 
 def isSymbol(s):
@@ -8,7 +9,7 @@ def isSymbol(s):
     return False
   for c in s:
     if c not in SYMBOLS:
-      if not c.alnum():
+      if not c.isalnum():
         return False
   return True
 
@@ -25,6 +26,9 @@ def fail(message, source_line, source_input_line):
 # parsing a line
 
 def parseLine(s, line_num):
+  destination = ''
+  comp = ''
+  jmp = ''
   result_list = []
   line = s
   if s.find("//") >= 0:
@@ -32,33 +36,54 @@ def parseLine(s, line_num):
   s = s.strip()
   if len(s) < 1:
     return None
-
   # check if address instruction
   if s[0] == '@':
-    s = [1:].strip()
+    s = s[1:].strip()
     if isSymbol(s) or isConstant(s):
-      result_list.append(('A_INSTRUCTION', s, line, line_num))
+      return str(('A_INSTRUCTION', s, line, line_num))
 
   # check if symbol 
   if s[0] == '(' and s[-1] == ')':
-    s = [1:-1]
+    s = s[1:-1]
     if isSymbol(s):
-      result_list.append(('L_INSTRUCTION', s, line, line_num))
+      return str(('L_INSTRUCTION', s, line, line_num))
 
-  # handle equals sign
+  # handle equals sign aka destination
   if '=' in s:
-    destination = s[s:s.find('=')]
-	rest = s[s.find('=')+1:]
-	# TODO: see if rest contains jump
-    if destination not in DESTINATIONS:
+    destination = s[:s.find('=')]
+    comp = s[s.find('=')+1:]
+    if destination not in destinationTable:
       fail("Invalid Destination", line_num, s)
   else:
     destination = ''
-
   # check if jump
+  print s
   if ';' in s:
-    jump = s[s.find(';')+1:]
-    if jump not in JUMPS:
-      fail("Invalid Jump", line_num, s)
+    comp = s[:s.find(';')]
+    jmp = s[s.find(';')+1:]
+    if jmp not in jumpTable:
+      fail("Invalid Jump", line_num, line)
+  elif ';' not in s:
+    jmp = ''
   else:
-    jump = ''
+    fail("Invalid Syntax", line_num, line)
+
+  return str(("C_INSTRUCTION", destination, comp, jmp, line, line_num))
+
+fn = open('../projects/06/rect/RectL.asm', 'r')
+
+output = []
+i = 0
+for line in fn:
+  s = parseLine(line, i)
+  if s == None:
+    continue
+  output.append(parseLine(line, i)+"\n")
+  i += 1
+
+fn.close()
+
+hack = open('output.hack', 'wr')
+for line in output:
+  hack.write(line)
+hack.close()
