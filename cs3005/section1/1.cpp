53f64a368fee91fe47e5d6e9eb13c677cb657528
Nate <natearmstrong2@gmail.com>
2013-03-05 18:55:27 -0700

    Restructured

--- cs3005/section1/1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main ()
-{
-    std::cout << "Ring around the rosie, pockets full of posie. Ashes, ashes, we all fall down!";
-    return 0;
-}
--- cs3005/section1/2.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main ()
-{
-    std::cout << 3*4+(7-5)*8;
-    return 0;
-}
