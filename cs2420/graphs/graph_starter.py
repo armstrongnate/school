b6ee6609cf82570452b2f7ff36d6863b277069ea
Nate <natearmstrong2@gmail.com>
2013-04-12 10:48:20 -0600

    Adds graphs; 2420.

---
+++ cs2420/graphs/graph_starter.py
@@ -0,0 +1,15 @@
+class Graph:
+  def __init__(self, vertex_count):
+    self.vertices =
+
+  def findPath(x, y):
+    pass
+
+  def addEdge(x, y):
+    pass
+
+  def getConnections(x):
+    pass
+
+  def isEdge(x, y):
+    pass
