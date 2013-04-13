ac287210bb4554a23a2865c445a543de301ac2bf
Nate <natearmstrong2@gmail.com>
2013-04-12 20:30:28 -0600

    Adds more graph notes and code; 2420.

--- cs2420/graphs/graph_starter.py
+++ cs2420/graphs/graph_starter.py
@@ -1,8 +1,37 @@
+# we will be using an Adjacency list to keep track of edges and vertices
+# example Adjacency list:
+
+## [[1,2], [0,2,3], [0,1,3], [1,2,4], [3], [6,7], [5,7], [5,6]]
+
+# example vertices list
+## [False, False, False, False, False, False, False, False]
+
 class Graph:
   def __init__(self, vertex_count):
-    self.vertices =
+    self.vertices = [False]*vertex_count
 
-  def findPath(x, y):
+  def findPathDepth(start, end):
+    # uses a stack
+    # always reaching straight down until we hit what we are looking for. Once a dead end, back up and try other direction.
+
+    # mark all vertices in vertices list (above) as not visited
+    s = Stack()
+    s.push(start)
+    vertices[start] = True
+    while not s.isEmpty():
+      c = s.top()
+      if c == end:
+        # build path and return items in stack as a list starting from bottom up. [4,3,2,1,0].reverse()
+      n = getConnections(c) # for 0, this will return [1,2]. see Adjacency list above.
+      # if all of n has been visited:
+        s.pop()
+      else:
+        s.push(f) # f being the first one that hasn't been visited
+        vertices[f] = True
+    return None
+
+  def findPathBreadth(x, y):
+    # uses a queue
     pass
 
   def addEdge(x, y):
