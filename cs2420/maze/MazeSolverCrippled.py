e6cfe8f211e13d47b7489af7a1b2ddd3c3c23f69
Nate <natearmstrong2@gmail.com>
2013-04-16 15:13:47 -0600

    Completes maze assignment for 2420.

--- cs2420/maze/MazeSolverCrippled.py
+++ cs2420/maze/MazeSolverCrippled.py
@@ -5,142 +5,181 @@
 from graphics import *
 import random
 
-M = 5
-N = 4
-CELL_SIZE = 100
+M = 30
+N = 30
+CELL_SIZE = 10
 MARGIN = 10
 screen_x = M*CELL_SIZE + 2*MARGIN
 screen_y = N*CELL_SIZE + 2*MARGIN
+sys.setrecursionlimit(100000)
 
 class Cell:
-    def __init__(self):
-        self.l = self.t = self.r = self.b = True
-        self.visited = False
+  def __init__(self):
+    self.l = self.t = self.r = self.b = True
+    self.visited = False
 
-    def Draw(self, win, i,j):
-        x1 = MARGIN + i*CELL_SIZE
-        y1 = MARGIN + j*CELL_SIZE
-        x2 = x1 + CELL_SIZE
-        y2 = y1 + CELL_SIZE
-        if self.l:
-            line = Line( Point(x1,y1), Point(x1,y2) )
-            line.draw(win)
-        if self.t:
-            line = Line( Point(x1,y1), Point(x2,y1) )
-            line.draw(win)
-        if self.r:
-            line = Line( Point(x2,y1), Point(x2,y2) )
-            line.draw(win)
-        if self.b:
-            line = Line( Point(x1,y2), Point(x2,y2) )
-            line.draw(win)
+  def Draw(self, win, i,j):
+    x1 = MARGIN + i*CELL_SIZE
+    y1 = MARGIN + j*CELL_SIZE
+    x2 = x1 + CELL_SIZE
+    y2 = y1 + CELL_SIZE
+    if self.l:
+      line = Line( Point(x1,y1), Point(x1,y2) )
+      line.draw(win)
+    if self.t:
+      line = Line( Point(x1,y1), Point(x2,y1) )
+      line.draw(win)
+    if self.r:
+      line = Line( Point(x2,y1), Point(x2,y2) )
+      line.draw(win)
+    if self.b:
+      line = Line( Point(x1,y2), Point(x2,y2) )
+      line.draw(win)
 class Maze:
-    def __init__(self):
-        self.cells = []
-        for i in range(M):
-            cellColumn = []
-            for j in range(N):
-                cellColumn.append(Cell())
-            self.cells.append(cellColumn)
-        self.VisitR(0,0)
-        self.cells[0][0].t = False
-        self.cells[M-1][N-1].b = False
-        
-    def VisitR(self, i,j):
-        self.cells[i][j].visited = True
-        while True:
-            nexti = []
-            nextj = []
-            # determine which cells we could move to next
-            if i>0 and not self.cells[i-1][j].visited: # left
-                nexti.append(i-1)
-                nextj.append(j)
-            if i<M-1 and not self.cells[i+1][j].visited: # right
-                nexti.append(i+1)
-                nextj.append(j)
-            if j>0 and not self.cells[i][j-1].visited: # up
-                nexti.append(i)
-                nextj.append(j-1)
-            if j<N-1 and not self.cells[i][j+1].visited: # down
-                nexti.append(i)
-                nextj.append(j+1)
+  def __init__(self):
+    self.cells = []
+    for i in range(M):
+      cellColumn = []
+      for j in range(N):
+        cellColumn.append(Cell())
+      self.cells.append(cellColumn)
+    self.VisitR(0,0)
+    self.cells[0][0].t = False
+    self.cells[M-1][N-1].b = False
 
-            if len(nexti) == 0:
-                return # nowhere to go from here
+  def VisitR(self, i,j):
+    self.cells[i][j].visited = True
+    while True:
+      nexti = []
+      nextj = []
+      # determine which cells we could move to next
+      if i>0 and not self.cells[i-1][j].visited: # left
+        nexti.append(i-1)
+        nextj.append(j)
+      if i<M-1 and not self.cells[i+1][j].visited: # right
+        nexti.append(i+1)
+        nextj.append(j)
+      if j>0 and not self.cells[i][j-1].visited: # up
+        nexti.append(i)
+        nextj.append(j-1)
+      if j<N-1 and not self.cells[i][j+1].visited: # down
+        nexti.append(i)
+        nextj.append(j+1)
 
-            # randomly choose 1 direction to go
-            index = random.randrange(len(nexti))
-            ni = nexti[index]
-            nj = nextj[index]
+      if len(nexti) == 0:
+        return # nowhere to go from here
 
-            # knock out walls between this cell and the next cell
-            if ni == i+1: # right move
-                self.cells[i][j].r = self.cells[i+1][j].l = False
-            if ni == i-1: # left move
-                self.cells[i][j].l = self.cells[i-1][j].r = False
-            if nj == j+1: # down move
-                self.cells[i][j].b = self.cells[i][j+1].t = False
-            if nj == j-1: # up move
-                self.cells[i][j].t = self.cells[i][j-1].b = False
+      # randomly choose 1 direction to go
+      index = random.randrange(len(nexti))
+      ni = nexti[index]
+      nj = nextj[index]
 
-            # recursively visit the next cell
-            self.VisitR(ni,nj)
-        
-    def Draw(self, win):
-        for i in range(M):
-            for j in range(N):
-                self.cells[i][j].Draw(win,i,j)
-        
+      # knock out walls between this cell and the next cell
+      if ni == i+1: # right move
+        self.cells[i][j].r = self.cells[i+1][j].l = False
+      if ni == i-1: # left move
+        self.cells[i][j].l = self.cells[i-1][j].r = False
+      if nj == j+1: # down move
+        self.cells[i][j].b = self.cells[i][j+1].t = False
+      if nj == j-1: # up move
+        self.cells[i][j].t = self.cells[i][j-1].b = False
 
-    # Write this method.
-    # It should return True if this is the end cell, OR if it leads to the end cell.
-    # It should return False if this is a loser cell.
-    def SolveR( self, i,  j):
-        # Mark this cell as visited.
+      # recursively visit the next cell
+      self.VisitR(ni,nj)
 
-        # Get index number of this cell
+  def Draw(self, win):
+    for i in range(M):
+      for j in range(N):
+        self.cells[i][j].Draw(win,i,j)
 
-	# Record the index in the class variable mMoves.
-        
-	# If we are at the end cell, return true.
-        
-	# move left if there is no wall, and it hasn't been visited. Return true if it returns true.
 
-        # move right if there is no wall, and it hasn't been visited. Return true if it returns true.
+  # Write this method.
+  # It should return True if this is the end cell, OR if it leads to the end cell.
+  # It should return False if this is a loser cell.
+  def SolveR( self, i,  j):
+    if i < 0 or j < 0 or i > M or j > N:
+      return False
+    # Mark this cell as visited.
+    self.cells[i][j].visited = True
+    # Get index number of this cell
+    index = j*M + i
+    end_cell_index = M*N-1
+    # Record the index in the class variable mMoves.
+    self.mMoves.append(index)
 
-	# move down if there is no wall, and it hasn't been visited. Return true if it returns true.
+    # If we are at the end cell, return true.
+    if index == end_cell_index:
+      return True
+    # move left if there is no wall, and it hasn't been visited. Return true if it returns true.
+    if not self.cells[i][j].l and not self.cells[i-1][j].visited:
+      winner = self.SolveR(i-1, j)
+      if winner:
+        return True
+    # move right if there is no wall, and it hasn't been visited. Return true if it returns true.
+    if not self.cells[i][j].r and not self.cells[i+1][j].visited:
+      winner = self.SolveR(i+1, j)
+      if winner:
+        return True
+  # move down if there is no wall, and it hasn't been visited. Return true if it returns true.
+    if not self.cells[i][j].b and not self.cells[i][j+1].visited:
+      winner = self.SolveR(i, j+1)
+      if winner:
+        return True
+  # move up if there is no wall, and it hasn't been visited. Return true if it returns true.
+    if not self.cells[i][j].t and not self.cells[i][j-1].visited:
+      winner = self.SolveR(i, j-1)
+      if winner:
+        return True
+  # This is a loser cell, so undo the move from self.mMoves, and return false to the previous cell.
+    self.mMoves.pop()
+    return False
 
-	# move up if there is no wall, and it hasn't been visited. Return true if it returns true.
+  # Write this method.
+  # Use a depth first search.
+  def Solve(self):
+  # Initialize mMoves array
+    self.mMoves = []
 
-	# This is a loser cell, so undo the move from self.mMoves, and return false to the previous cell.
-        return False
+  # Initialize all self.cells to not visited
+    for i in range(M):
+      for j in range(N):
+        self.cells[i][j].visited = False
 
-    # Write this method.
-    # Use a depth first search.
-    def Solve(self):
-	# Initialize mMoves array
-        self.mMoves = []
+    # Start searching recursively from 0,0
+    for i in range(M):
+      for j in range(N):
+        self.SolveR(i, j)
 
-	# Initialize all cells to not visited
-	
-	# Start searching recursively from 0,0
-
-    # Write this method.
-    def DrawSolution(self, win):
-        print self.mMoves
-        
-        # Now draw it graphically!
-           
+  # Write this method.
+  def DrawSolution(self, win):
+    # Now draw it graphically!
+    for i in range(len(self.mMoves)-1):
+      i1 = self.mMoves[i]%M
+      j1 = self.mMoves[i]/M
+      i2 = self.mMoves[i+1]%M
+      j2 = self.mMoves[i+1]/M
+      x1 = MARGIN + CELL_SIZE/2 + CELL_SIZE * i1
+      y1 = MARGIN + CELL_SIZE/2 + CELL_SIZE * j1
+      x2 = MARGIN + CELL_SIZE/2 + CELL_SIZE * i2
+      y2 = MARGIN + CELL_SIZE/2 + CELL_SIZE * j2
+      line = Line(Point(x1, y1), Point(x2, y2))
+      line.setFill("red")
+      line.draw(win)
 
 
 def main():
-    win = GraphWin("Maze Solver", screen_x, screen_y)
+  win = GraphWin("Maze Solver", screen_x, screen_y)
 
-    theMaze = Maze()
-    theMaze.Draw(win)
-    theMaze.Solve()
-    theMaze.DrawSolution(win)
+  theMaze = Maze()
+  theMaze.Draw(win)
+  x = 0
+  while x < 100000:
+    x += .25
+  theMaze.Solve()
+  theMaze.DrawSolution(win)
+  p = win.getMouse()
+  win.close()
 
 main()
 
-    
+
