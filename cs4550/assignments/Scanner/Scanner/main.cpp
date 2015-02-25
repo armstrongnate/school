711be86c3efa67fae15dd2c1d2a4984fc92d3a55
Nate Armstrong <natearmstrong2@gmail.com>
2015-02-25 15:11:51 -0700

    cs4550: add program 4 - symbol table

--- cs4550/assignments/Scanner/Scanner/main.cpp
+++ cs4550/assignments/Scanner/Scanner/main.cpp
@@ -7,16 +7,48 @@
 //
 
 #include <iostream>
+#include <assert.h>
+
 #include "Token.h"
 #include "Scanner.h"
+#include "SymbolTable.h"
 
-int main(int argc, const char * argv[]) {
+void scan() {
   Scanner scanner("/Users/nate/school/cs4550/assignments/Scanner/Scanner/main.txt");
   Token token;
   do {
     token = scanner.GetNextToken();
     cout << token << " " << scanner.GetLineNumber() << endl;
   } while (token.GetTokenType() != EOF_TOKEN);
+}
 
+void testSymbolTable() {
+  SymbolTable table;
+
+  // Exists, AddEntry, GetCount
+  assert(table.GetCount() == 0);
+  assert(!table.Exists("foo"));
+  table.AddEntry("foo");
+  assert(table.Exists("foo"));
+  assert(table.GetCount() == 1);
+
+  // GetIndex of 'foo' should be 0
+  assert(table.GetIndex("foo") == 0);
+
+  // GetValue for 'foo' should be the default, 0
+  assert(table.GetValue("foo") == 0);
+
+  // SetValue
+  table.SetValue("foo", 1);
+  assert(table.GetValue("foo") == 1);
+
+  // Failures
+//  table.AddEntry("foo"); // variable already exists
+//  table.SetValue("bar", 3); // variable does not exist
+  table.GetValue("baz"); // variable does not exist
+}
+
+int main(int argc, const char * argv[]) {
+  scan();
   return 0;
 }
