85031119553d8b61c9c04fca4cdf05061c8a03bf
Nate <natearmstrong2@gmail.com>
2015-11-29 11:54:35 -0700

    Add a few words for forth assignment

--- cs3520/assignments/forth/missionaries.fs
+++ cs3520/assignments/forth/missionaries.fs
@@ -1,2 +1,7 @@
-: 3dup ( x y z -- x y z x y z x y z ) todo ;
+: 3dup ( x y z -- x y z x y z x y z )
+    dup     ( x y z z )
+    2over   ( x y z z x y )
+    rot     ( x y z x y z )
+  ;
+
 : 3drop ( x y z -- ) drop drop drop ;
